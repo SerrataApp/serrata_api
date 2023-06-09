ab8d4ed199715f7196cb3b2bb8f89ae7c68b0132
CorentinAT <corentin.al2004@gmail.com>
2023-06-09 15:54:55 +0200

    creation des tables et des insertions de score

--- bdd.py
+++ bdd.py
@@ -2,7 +2,6 @@ import sqlite3
 from sqlite3 import Error
 
 def create_connection(db_file):
-  """Créer une connexion à un fichier de base de données"""
   conn = None
   try:
     conn = sqlite3.connect(db_file)
@@ -12,18 +11,62 @@ def create_connection(db_file):
   return conn
   
 def create_table(conn, create_table_sql):
-  """Créer une table depuis la requête SQL donnée"""
   try:
     c = conn.cursor()
     c.execute(create_table_sql)
   except Error as e:
     print(e)
 
+def create_score_europe(conn, score, joueur):
+  sql = "SELECT MAX(id) FROM ScoresEurope;"
+  cur = conn.cursor()
+  cur.execute(sql)
+  idMax = cur.fetchall()[0][0]
+  if(idMax == None):
+    sql = "INSERT INTO ScoresEurope (idScore, temps, joueur) VALUES (1, ?, ?)"
+  else:
+    sql = "INSERT INTO ScoresEurope (idScore, temps, joueur) VALUES (?, ?, ?)"
+    data = (idMax + 1, score, joueur)
+  cur = conn.cursor()
+  cur.execute(sql, data)
+  conn.commit()
+  return cur.lastrowid
+
+def create_score_onu(conn, score, joueur):
+  sql = "SELECT MAX(id) FROM ScoresEurope;"
+  cur = conn.cursor()
+  cur.execute(sql)
+  idMax = cur.fetchall()[0][0]
+  if(idMax == None):
+    sql = "INSERT INTO ScoresOnu (idScore, temps, joueur) VALUES (1, ?, ?)"
+  else:
+    sql = "INSERT INTO ScoresOnu (idScore, temps, joueur) VALUES (?, ?, ?)"
+    data = (idMax + 1, score, joueur)
+  cur = conn.cursor()
+  cur.execute(sql, data)
+  conn.commit()
+  return cur.lastrowid
+
 def main():
   database = r"bdd.db"
+
+  sql_create_scores_europe_table = """
+  CREATE TABLE IF NOT EXISTS ScoresEurope (
+    idScore INT PRIMARY KEY,
+    temps INT,
+    joueur VARCHAR
+  );"""
+
+  sql_create_scores_onu_table = """
+  CREATE TABLE IF NOT EXISTS ScoresOnu (
+    idScore INT PRIMARY KEY,
+    temps INT,
+    joueur VARCHAR
+  );"""
+
   conn = create_connection(database)
   if conn is not None:
-    continue
+    create_table(conn, sql_create_scores_europe_table)
   else:
     print("Error, can't create the database connection")
 
