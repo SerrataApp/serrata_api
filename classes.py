1136eb9b62dc59860ee033b5c9f177b0e5b598c0
TangoCh4rile <loulou.reyms@orange.fr>
2023-11-01 17:43:08 +0100

    Ajout de classes pour l'authentification

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Union
 
 class Score(BaseModel):
   temps: int
@@ -8,3 +9,22 @@ class Score(BaseModel):
 def to_object_score(score):
   objScore = Score(temps=score[0], erreurs=score[1], joueur=score[2])
   return objScore
+
+# Classes relatives à l'authentification
+
+class Token(BaseModel):
+  access_token: str
+  token_type: str
+
+class TokenData(BaseModel):
+  username: Union[str, None] = None
+
+
+class User(BaseModel):
+  username: str
+  email: Union[str, None] = None
+  full_name: Union[str, None] = None
+  disabled: Union[bool, None] = None
+
+class UserInDB(User):
+  hashed_password: str
