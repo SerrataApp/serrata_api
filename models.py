59dedc09b11d1566998331322aa6f8a223a5f793
TangoCh4rlie <elouanreymond@tutanota.com>
2023-11-02 21:46:46 +0100

    Ajout de la classe Game

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date, Time
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -15,4 +15,16 @@ class User(Base):
     signupDate = Column(Date)
     is_active = Column(Boolean, default=True)
 
-    wonGames = relationship("Game", back_populates="playerName")
+    wonGames = relationship("Game", back_populates="playerPseudo")
+
+
+class Game(Base):
+    __tablename__ = "games"
+
+    id = Column(Integer, primary_key=True, index=True)
+    time = Column(Time, index=True)
+    errors = Column(Integer)
+    hint = Column(Integer)
+    gameDate = Column(Date)
+
+    playerPseudo = relationship("User", back_populates="wonGames")
