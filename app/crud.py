c46d7b9f64cb4d524e4bc2a3cc2a5ecd66755f0e
TangoCh4rile <loulou.reyms@orange.fr>
2023-11-23 16:47:19 +0100

    close #5; Ajout d'un sel plus création variable env

--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,9 @@
 from sqlalchemy.orm import Session
 
-
-from . import models, schemas
-
 from datetime import datetime, timedelta
 from typing import Annotated, Union
+import os
+from dotenv import load_dotenv
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -12,10 +11,15 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 
 from .get_db import get_db
+from . import models, schemas
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+SEL = os.getenv("SEL")
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -23,12 +27,11 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password + SEL, hashed_password)
 
 
 def get_password_hash(password):
-    #TODO: Faire un petit sellllllll
-    return pwd_context.hash(password)
+    return pwd_context.hash(password + SEL)
 
 
 def get_user_by_username(db: Session, username: str):
